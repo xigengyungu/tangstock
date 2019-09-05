88432a1b1097887a551fde5a7468173fb8f1c4d3
gujingyun2000 <gujingyun2000@126.com>
2019-09-05 08:10:55 +0800

    添加引入rest_framework，指定渲染器为Json和rest_framework网页渲染器

--- api/views/course.py
+++ api/views/course.py
@@ -1,7 +1,13 @@
 __author__ = 'gujingyun'
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.shortcuts import HttpResponse
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 
 class CourseView(APIView):
+    #指定返回类型为Json
+    renderer_classes = [JSONRenderer,BrowsableAPIRenderer,]
+
     def get(self, request, *args, **kwargs):
         return Response('...')
+        #return HttpResponse('...')
