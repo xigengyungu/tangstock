bbee50237b7148dbe1207190dfff3635e490fa32
gujingyun2000 <gujingyun2000@126.com>
2019-09-23 07:21:11 +0800

    修正匹配不到路由的问题

--- api/urls.py
+++ api/urls.py
@@ -7,5 +7,5 @@ from api.views import course
 urlpatterns = [
     #path('admin/', admin.site.urls),
     url(r'^course/$', course.CourseView.as_view()),
-    url(r'^course/(P<pk>\d+)/$', course.CourseView.as_view()),
+    url(r'^course/(?P<pk>\d+)/$', course.CourseView.as_view()),
 ]
