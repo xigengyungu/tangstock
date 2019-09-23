a8b7865a20a6dfe81828dde81a2a90a026ece466
gujingyun2000 <gujingyun2000@126.com>
2019-09-23 08:29:12 +0800

    通过自定义字段取关联表字段，自定义方法取多对多关联字段

--- api/views/course.py
+++ api/views/course.py
@@ -16,10 +16,21 @@ class CoursesSerializer(serializers.ModelSerializer) :
         fields = ["id", "name", "course_img", "level"]
 
 class CourseDetailSerializer(serializers.ModelSerializer):
+    #o2o o2m
+    title = serializers.CharField(source='course.name')
+    img = serializers.CharField(source='course.course_img')
+    level = serializers.CharField(source='course.get_level_display')
+
+    #m2m
+    recommends = serializers.SerializerMethodField()
     class Meta:
         model = models.CourseDetail
-        fields = "__all__"
-        depth = 2
+        fields = ["course", "title", "img", "level", "course_slogan", "why_study", "recommends"]
+        #depth = 2
+
+    def get_recommends(self, obj):
+        queryset = obj.recommend_courses.all()
+        return [{'id':row.id, 'title':row.name} for row in queryset]
 
 class CourseView1(APIView):
     #指定返回类型为Json
